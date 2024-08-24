699a50316ba2c7201c4f95dde1872696fd4f336c
samuel <samuelfernandes8605@gmail.com>
2024-08-24 21:06:10 +0530

    new

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Banner = () => {
   return (
     <div className="banner bg-[#121c30] px-2 py-2 flex justify-center items-center">
-      <p className="text-white text-sm font-semibold px-2">Stake raises $14 million in Series A funding!</p> <p className="text-blue-500 font-semibold">Read more</p>
+      <p className="text-white text-sm font-semibold px-2">Stake raises Rs 14 million in Series A funding!</p> <p className="text-blue-500 font-semibold">Read more</p>
     </div>
   )
 }
